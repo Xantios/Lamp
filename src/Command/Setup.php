eef41d2ae68b19f6b1b56d20b4a45dbe7c1ce57e
Xantios Krugor <git@xantios.nl>
2021-11-30 21:21:05 +0100

    Added some exception handling and finished up registration flow

--- src/Command/Setup.php
+++ src/Command/Setup.php
@@ -2,14 +2,17 @@
 
 namespace Xantios\Lamp\Command;
 
+use GuzzleHttp\Exception\GuzzleException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use Xantios\Lamp\Config;
+use Xantios\Lamp\Hue\ApiClient;
 use Xantios\Lamp\Hue\Hub;
 use Xantios\Lamp\Hue\HubArray;
+use Xantios\Lamp\Types\ipv4;
 
 class Setup extends Command
 {
@@ -32,9 +35,21 @@ class Setup extends Command
             $output->writeln("<info>Using defined IP ${hub}</info>");
         } else {
 
-            $hubs = $this->hub->search();
+            try {
+                $hubs = $this->hub->search();
+            } catch (GuzzleException $e) {
+                $output->writeln("<error>Cant reach Philips API servers.</error>");
+                return Command::FAILURE;
+            } catch (\JsonException $e) {
+                $output->writeln("<error>API server spat out gibberish.</error>");
+                return Command::FAILURE;
+            } catch (\Exception $e) {
+                $output->writeln("<error>Something went wrong, please try again.</error>");
+                return Command::FAILURE;
+            }
+
             if (count($hubs) <= 0) {
-                $output->writeLn("<error>No hubs found</error>");
+                $output->writeLn("<error>No hubs found, please make sure its on and ready</error>");
                 return Command::FAILURE;
             }
 
@@ -45,7 +60,21 @@ class Setup extends Command
             }
         }
 
-        $output->writeln("<error>blaat ${hub}</error>");
+        $output->writeln("<info>Connected to $hub Please press the link button on your hub.</info>");
+
+        $client = new ApiClient(new ipv4($hub));
+        do {
+            $register = $client->register();
+            sleep(1);
+            $output->write("<info>.</info>");
+        } while($register->error === true);
+        $output->writeln("");
+
+        $this->config->username = $register->data->success->username;
+        $this->config->hub = new ipv4($hub);
+        $path = $this->config->store();
+
+        $output->writeln("<info>Linked successfully! The config is stored to $path</info>");
 
         return Command::SUCCESS;
     }
